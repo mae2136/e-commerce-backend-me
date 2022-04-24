8fd66aaf453e9792c7795bc3cd45aeb5337ac8bc
Miguel Escobar <mae2136@gmail.com>
2022-04-24 16:02:55 -0500

    V 0.31 - Update json package with dependencies. - Change ondelete for Category - Product from "Set Null" to "Cascade". - Create Get routes for category routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,39 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    // Send error if no data returned for that id.
+    if (!categoryData) {
+      res.status(404).json({ message: `No category found with that id!`});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
