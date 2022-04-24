218892c9d4429e6db390c19ecc216cde7276688d
Miguel Escobar <mae2136@gmail.com>
2022-04-24 14:16:04 -0500

    V 0.2 - Updated Models for sequelize to create database.

--- models/Product.js
+++ models/Product.js
@@ -9,9 +9,45 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: `category`,
+        key: `id`,
+      },
+    }, 
   },
   {
+    hooks: {
+      beforeValidate: async (newProduct) => {
+        newProduct.stock = 10;
+        return newProduct;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
